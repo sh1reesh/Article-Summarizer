7807e0f1522ac87c930a273ae23f552c1c5152f4
Shireesh <shenoy.shireesh@gmail.com>
2024-09-06 15:22:55 +0530

    Initial commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+
+import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type="url"
+            placeholder='Enter a URL'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className='url_input peer'
+          />
+
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
+          >
+            Submit
+          </button>
+        </form>
+
+        {/* Display Error */}
+        {error && (
+          <p className="mt-2 text-red-500">
+            Error: {error.data?.message || 'Something went wrong'}
+          </p>
+        )}
+
+        {/* Loading Spinner */}
+        {isFetching && (
+          <div className="flex justify-center items-center">
+            <img src={loader} alt="loading" className="w-6 h-6 mt-2" />
+          </div>
+        )}
+
+        {/* Display Results */}
+        {article.summary && (
+          <div className="mt-4">
+            <h3 className="font-bold text-lg">Summary:</h3>
+            <p className="mt-2">{article.summary}</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
